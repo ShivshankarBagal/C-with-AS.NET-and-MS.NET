83e4e44cc48cabb4fdadee01ffbf618c1f4c8303
shiv <shivshankarbagal1116@gmail.com>
2024-07-10 11:11:08 +0530

    Basic C#

---
+++ Day1/BasicClassConcepts/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicClassConcepts
+{
+    internal class Program
+    {
+        public void Text() {
+
+            System.Console.WriteLine("This is text()");
+        
+        }
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello world");
+            Console.WriteLine(2 + 8);
+
+            
+            Class1.Display();
+            Class1.Display(a:22,11);
+
+            Program p = new Program();
+
+            p.Text();
+          
+            Console.ReadLine();
+
+            
+        }
+        
+    }
+    public  static class Class1 {
+
+        public static void Display() {
+
+            Console.WriteLine("Display() method");
+        }
+        public static void Display(int a, int b)
+        {
+
+            Console.WriteLine("Display(String) method"+"A"+a+"B"+b);
+            LocaFun();
+          void LocaFun() { 
+
+                System.Console.WriteLine("Local function");
+            }
+        }
+    }
+
+}
---
+++ Day3/Day3/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3
+{
+
+
+    public class Pro2 {
+
+        private int id= 12;
+        private string name;
+        private int age;
+
+        public int Id { get; private set; }
+        public string Name { get { return name; } set { name = value; } }   
+
+        public int Age { get { return age; }
+            set {
+
+                if (value < 21 || value > 65)
+                    throw new ArgumentOutOfRangeException(" Sorry, You are not eligible for this position");               
+            age = value;
+            }
+        
+        }
+        
+    
+    }
+    internal class Program
+    {
+
+        public static int e;
+
+        Program() { }
+        static void Main(string[] args)
+        {
+           
+            //System.Console.WriteLine("This is main() method");
+            Pro2 p = new Pro2();
+            try
+            {
+              //  p.Name = "test";
+                p.Age = 33;
+
+               // System.Console.WriteLine("Id " + p.Id);
+                //System.Console.WriteLine("Name " + p.Name);
+                System.Console.WriteLine("Age" +  p.Age);
+            }
+            catch(ArgumentOutOfRangeException ag)
+            {
+                Console.WriteLine(ag.Message);
+
+            }
+            
+        }
+        
+        
+    }
+
+    public class Class1 { 
+    
+    
+    
+    
+    }
+}
