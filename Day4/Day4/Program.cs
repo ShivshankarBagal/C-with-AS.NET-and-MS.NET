37c908b8011ccdc9784f458bb1d87fafd6b78eb7
shiv <shivshankarbagal1116@gmail.com>
2024-07-11 23:31:27 +0530

    method overriding and Anonymouse method

--- Day4/Day4/Program.cs
+++ Day4/Day4/Program.cs
@@ -30,77 +30,98 @@ namespace Day4
 
     internal class Program
     {
-       /* static void Main(string[] args)
-        {
-            Dog myDog = new Dog();
-            myDog.Name = "Buddy";
+        /* static void Main(string[] args)
+         {
+             Dog myDog = new Dog();
+             myDog.Name = "Buddy";
 
-            // Accessing methods from the base class
-            myDog.Eat();
-            myDog.Sleep();
+             // Accessing methods from the base class
+             myDog.Eat();
+             myDog.Sleep();
 
-            // Accessing method from the derived class
-            myDog.Bark();
-        }*/
+             // Accessing method from the derived class
+             myDog.Bark();
+         }*/
     }
 
 
-    internal class Base { 
-    
-    
-     public virtual String Name { get; set; }
-    
-    }
-    internal class Derived : Base
+
+
+    class baseClass
     {
 
-        public String _name;
-
-        public override String Name { 
-        
-
-            get { return base.Name; }
-            set { base.Name = value+"Base"; 
-                 _name = value;
-            }
-            
+        // show() is 'virtual' here
+        public virtual void show()
+        {
+            Console.WriteLine("Base class");
         }
-
-        public String DerivedNumber { 
-        get { return _name; }
-                
-                
-                
-                }
-        
-
-        
-
-
-
     }
 
-    
 
+    // class 'baseClass' inherit 
+    // class 'derived'
+    class derived : baseClass
+    {
 
-    internal class Pro4 {
-
-        static void Main(string[] args) {
-
-
-            Derived derived = new Derived();
-            derived.Name = "abc";
-            System.Console.WriteLine(derived.Name);
-            System.Console.WriteLine(derived.DerivedNumber);
-
-
-
+        //'show()' is 'override' here
+        public override void show()
+        {
+            Console.WriteLine("Derived class");
         }
+    }
+
+    class GFG
+    {
+
+        // Main Method
+/*        public static void Main()
+        {
+
+            baseClass obj;
+
+            // 'obj' is the object
+            // of class 'baseClass'
+            obj = new baseClass();
+
+            // it invokes 'show()' 
+            // of class 'baseClass'
+            obj.show();
 
 
+            // the same object 'obj' is now
+            // the object of class 'derived'
+            obj = new derived();
 
+            // it invokes 'show()' of class 'derived'
+            // 'show()' of class 'derived' is overridden
+            // for 'override' modifier
+            obj.show();
 
+        }*/
+    }
+    class AnonymousMethod
+    {
 
+        // Create a delegate 
+        public delegate void petanim(string pet);
 
+        // Main method 
+        static public void Main()
+        {
+
+            string fav = "Rabbit";
+
+            // Anonymous method with one parameter 
+            petanim p = delegate (string mypet)
+            {
+                Console.WriteLine("My favorite pet is {0}.",
+                                                     mypet);
+
+                // Accessing variable defined 
+                // outside the anonymous function 
+                Console.WriteLine("And I like {0} also.", fav);
+            };
+            p("Dog");
+        }
     }
 }
